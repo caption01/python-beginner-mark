cb2cd0ca04fe4af6854dd7155d8d6439ceb02ecc
APPMAN M-095 <nutchapon.han@gmail.com>
2021-07-20 22:04:15 +0700

    string format

---
+++ string_manipulation/format_function.py
@@ -0,0 +1,5 @@
+name = 'Bom'
+age = 26
+message = 'Hello {name} ({age})'.format(age = age, name = name)
+
+print(message)
---
+++ string_manipulation/formatted_string_literal.py
@@ -0,0 +1,5 @@
+name = 'Bom'
+age = 26
+message = f'Hello {name} {age}'
+
+print(message)
