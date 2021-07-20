dbf90f6f0bee2aa06c17c3b9da353e7b457f1e4c
APPMAN M-095 <nutchapon.han@gmail.com>
2021-07-20 22:04:27 +0700

    condition

---
+++ control_flow/if_statement.py
@@ -0,0 +1,10 @@
+age = 17
+
+if age <= 14:
+    print('Sorry you must be over the age of 14 to use this tools.')
+elif age < 18:
+    print('Users under the age of 18 must get the permission.')
+else:
+    print('Welcome to this tools!')
+
+print('i am not is scope indention!!')
